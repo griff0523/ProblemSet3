8bc1be7c4ff2d77fbea79681118d3f8637d9009d
griff0523 <griffin.bird@duke.edu>
2023-10-19 18:48:51 -0400

    Finished Problem Set 3 part 2, committing changes.

--- ProblemSet3_Part2.py
+++ ProblemSet3_Part2.py
@@ -53,4 +53,41 @@ if vesselID in vesselDict:
     country = vesselDict[vesselID]
     print(f"Vessel # {vesselID} flies the flag of {country}.")
 
-# %%
+#%% Task 5 Scripting Task
+#Reading in Loitering Events csv file
+fileObj2 = open(file='data/raw/loitering_events_20180723.csv',mode='r')
+
+#Read the entire contents into a list object
+loiterlineList = fileObj2.readlines()
+
+#Closing the csv file as all contents have now been read in
+fileObj2.close() #Close the file
+
+#For loop that will go through all lines from the loitering csv file
+for items in loiterlineList[1:]:
+    #Creating boolean variables for the latitude and longitude specifications that will 
+    #reiterate for all lines
+    equator_cross = False
+    long_rule = False
+    
+    #Splitting lines from csv files into separate string entrys, delineating by ","
+    entries = items.split(",")
+    
+    #Telling python where to look for the values for each variable
+    t_mmsi = entries[0]
+    start_lat = float(entries[1])
+    start_long = float(entries[2])
+    end_lat = float(entries[3])
+    end_long = float(entries[4])
+   
+    #Creating latitude boolean statement
+    if start_lat < 0 and end_lat > 0:
+        equator_cross = True
+   
+    #Creating rule for longitude parameter
+    if start_long >= 165 and start_long <= 170:
+        long_rule = True
+    
+    #If statement selecting filtering vessels by whether thy cross the equator AND are between 165 and 170 longitude
+    if equator_cross and long_rule:
+        print(f"Vessel #{t_mmsi} flies the flag of {vesselDict[t_mmsi]}.")
